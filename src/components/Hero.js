7b518252ba4d88fd68717e76aed17d72179a5167
Charles Bayley <cbayley95@gmail.com>
2018-08-11 16:25:55 -0400

    Added Hero

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+class Hero extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is the hero component</p>
+      </div>
+    );
+  }
+}
+
+export default Hero;
+

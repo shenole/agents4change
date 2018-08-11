6b1ef77352182616aaca2f86e2f11b1d63e51513
Charles Bayley <cbayley95@gmail.com>
2018-08-11 16:28:30 -0400

    Added Resources

---
+++ src/components/Resources.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+class Resources extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is the resources component</p>
+      </div>
+    );
+  }
+}
+
+export default Resources;
+

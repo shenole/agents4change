c56aede23894bb5f4162f6de3135daa074f14b6f
Charles Bayley <cbayley95@gmail.com>
2018-08-11 16:26:51 -0400

    Added About

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is the about component</p>
+      </div>
+    );
+  }
+}
+
+export default About;
+

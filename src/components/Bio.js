f4c4d4551a0e13578635fc92c458ec4e0a9e07c7
Charles Bayley <cbayley95@gmail.com>
2018-08-11 16:27:49 -0400

    Added Bio

---
+++ src/components/Bio.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+class Bio extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is the bio component</p>
+      </div>
+    );
+  }
+}
+
+export default Bio;
+

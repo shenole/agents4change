68817ae07d6c80ee253803da7672b767522f9b63
Charles Bayley <cbayley95@gmail.com>
2018-08-11 16:17:03 -0400

    Added Header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is the header component</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
+
